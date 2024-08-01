1ae398c683ee5cb581f78489b20f816d3b3245d1
vizionnn <joaovictorblima@hotmail.com>
2024-07-31 22:45:48 -0300

    correção 3

--- bot-vanilla.py
+++ bot-vanilla.py
@@ -22,116 +22,102 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 cargo_em_analise_id = 1267282437315104911
 cargo_membro_id = 1267282437315104917
 canal_prova_aluno_id = 1267282438074273794
-moderator_roles_ids = [
-    1267282437411700828,
-    1267282437411700827,
-    1267282437411700829,
-    1267282437352849539,
-]
+canal_corrigir_prova_id = 1235035965945413649
+cargo_visualizacao_1_id = 1267282437411700828
+cargo_visualizacao_2_id = 1267282437411700829
+cargo_prova_id = 1267282437352849539
 
-# Questões da prova
 questoes_abertas = [
     "Qual seu nome na cidade?",
     "Qual seu passaporte na cidade (ID)?",
 ]
 
-# Arquivo para armazenar o ID da mensagem inicial
-ID_ARQUIVO = 'mensagem_inicial_id.json'
-
-def salvar_id_mensagem(mensagem_id):
-    with open(ID_ARQUIVO, 'w') as f:
-        json.dump({'mensagem_id': mensagem_id}, f)
-
-def carregar_id_mensagem():
-    if os.path.exists(ID_ARQUIVO):
-        with open(ID_ARQUIVO, 'r') as f:
-            data = json.load(f)
-            return data.get('mensagem_id')
-    return None
-
-class ProvaView(discord.ui.View):
-    def __init__(self, bot, user):
-        super().__init__(timeout=None)
+class ProvaView(View):
+    def __init__(self, bot):
+        super().__init__()
         self.bot = bot
-        self.user = user
 
     @discord.ui.button(label="Realizar Prova", style=discord.ButtonStyle.green)
-    async def realizar_prova(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def realizar_prova(self, interaction: discord.Interaction, button: Button):
         await interaction.response.send_message("Iniciando sua prova!", ephemeral=True)
         overwrites = {
             interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False),
-            interaction.user: discord.PermissionOverwrite(read_messages=True, send_messages=True)
+            interaction.user: discord.PermissionOverwrite(read_messages=True, send_messages=True),
+            interaction.guild.get_role(cargo_visualizacao_1_id): discord.PermissionOverwrite(read_messages=True),
+            interaction.guild.get_role(cargo_visualizacao_2_id): discord.PermissionOverwrite(read_messages=True),
+            interaction.guild.get_role(cargo_prova_id): discord.PermissionOverwrite(read_messages=False)
         }
-        for role_id in moderator_roles_ids:
-            role = interaction.guild.get_role(role_id)
-            overwrites[role] = discord.PermissionOverwrite(read_messages=True)
-        
         channel = await interaction.guild.create_text_channel(f"prova-{interaction.user.display_name}", overwrites=overwrites)
         await iniciar_prova(interaction.user, channel)
 
 async def iniciar_prova(user, channel):
     respostas = {}
-    try:
-        for questao in questoes_abertas:
-            embed = discord.Embed(title="Questão Aberta", description=f"{questao}", color=discord.Color.light_grey())
-            await channel.send(embed=embed)
+
+    for questao in questoes_abertas:
+        embed = discord.Embed(title="Questão Aberta", description=f"{questao}", color=discord.Color.light_grey())
+        await channel.send(embed=embed)
+        try:
             msg = await bot.wait_for('message', timeout=180.0, check=lambda m: m.author == user)
             respostas[questao] = msg.content
-            await channel.purge(limit=100)
-        
-        nome_na_cidade = respostas.get(questoes_abertas[0], "não respondida")
-        id_na_cidade = respostas.get(questoes_abertas[1], "não respondida")
+        except asyncio.TimeoutError:
+            respostas[questao] = "não respondida"
+            await channel.send("Tempo esgotado! Você precisará iniciar outra prova.")
+            await asyncio.sleep(10)
+            await channel.delete()
+            return
+        await channel.purge(limit=100)
 
-        if nome_na_cidade != "não respondida" and id_na_cidade != "não respondida":
-            novo_nome = f"{nome_na_cidade} #{id_na_cidade}"
-            await user.edit(nick=novo_nome)
-            await user.remove_roles(discord.Object(id=cargo_em_analise_id))
-            await user.add_roles(discord.Object(id=cargo_membro_id))
-        
-        await channel.send("Prova concluída! O canal será fechado em 10 segundos.")
-    except asyncio.TimeoutError:
-        await channel.send("Tempo esgotado! Você precisará iniciar outra prova.")
-    finally:
-        await asyncio.sleep(10)
-        await channel.delete()
+    _corrigir_prova = bot.get_channel(canal_corrigir_prova_id)
+    if _corrigir_prova:
+        embed = discord.Embed(title="Relatório de Prova", color=discord.Color.light_grey())
+        embed.add_field(name="Prova realizada por", value=f"{user.mention} | {user.id}", inline=False)
+
+        for questao in questoes_abertas:
+            embed.add_field(name=questao, value=f"R: {respostas.get(questao, 'não respondida')}", inline=False)
+
+        embed.set_thumbnail(url="https://media.discordapp.net/attachments/1137047344903372941/1238973673671622666/marvel_gif_2.gif")
+        await _corrigir_prova.send(embed=embed)
+
+    await channel.send("Prova concluída! Contate um gerente. O canal será fechado em 10 segundos.")
+    await asyncio.sleep(10)
+    await channel.delete()
 
 async def enviar_ou_editar_mensagem_inicial():
     canal_prova_aluno = bot.get_channel(canal_prova_aluno_id)
     if canal_prova_aluno:
-        mensagem_inicial_id = carregar_id_mensagem()
         mensagem_inicial = None
-
-        if mensagem_inicial_id:
-            try:
-                mensagem_inicial = await canal_prova_aluno.fetch_message(mensagem_inicial_id)
-            except discord.NotFound:
-                mensagem_inicial = None
+        async for message in canal_prova_aluno.history(limit=10):
+            if message.author == bot.user and message.embeds and message.embeds[0].title == "Vanilla: Prova":
+                mensagem_inicial = message
+                break
 
         embed = discord.Embed(title="Vanilla: Prova", description="Você terá 3 minutos para iniciar a prova após clicar no botão.", color=discord.Color.light_grey())
         embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/1215880368167718993/c274073554a24903ae2aa8f51e38a635.webp")
         embed.set_image(url="https://cdn.discordapp.com/attachments/1218681672699220108/1226962867022987395/gif_animado.gif?ex=66abd4b7&is=66aa8337&hm=d351447bebc168606741b0b79d9051bd611239e6ecd3c76582fc6d20dcbcfdf6&")
         embed.set_footer(text="Faça sua parte e se junte a maior família da cidade!")
 
-        view = ProvaView(bot, None)
+        view = ProvaView(bot)
 
         if mensagem_inicial:
             await mensagem_inicial.edit(embed=embed, view=view)
         else:
-            nova_mensagem = await canal_prova_aluno.send(embed=embed, view=view)
-            salvar_id_mensagem(nova_mensagem.id)
+            await canal_prova_aluno.send(embed=embed, view=view)
 
-@tasks.loop(minutes=3)
+@tasks.loop(minutes=2)
 async def verificar_interacao():
     await enviar_ou_editar_mensagem_inicial()
 
 @bot.event
-async def on_member_join(member):
-    cargo_em_analise = member.guild.get_role(cargo_em_analise_id)
-    await member.add_roles(cargo_em_analise)
-
-@bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+    print(f'Bot conectado como {bot.user}')
+
+    # Sincronizar comandos do bot
+    await bot.tree.sync()
+
+    # Enviar ou editar a mensagem inicial da prova
+    await enviar_ou_editar_mensagem_inicial()
+
+    # Iniciar a verificação de interação
     if not verificar_interacao.is_running():
         verificar_interacao.start()
 
